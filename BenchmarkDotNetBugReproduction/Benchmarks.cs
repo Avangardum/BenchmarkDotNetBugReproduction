aca2efa34eba06442253ad23bb88696ffc7b1c3c
Avangardum <yurykuz65@gmail.com>
2023-11-13 16:47:37 +0300

    Initial commit

---
+++ BenchmarkDotNetBugReproduction/Benchmarks.cs
@@ -0,0 +1,14 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+
+namespace BenchmarkDotNetBugReproduction;
+
+[SimpleJob(RuntimeMoniker.NativeAot70)]
+public class Benchmarks
+{
+    [Benchmark]
+    public void Foo()
+    {
+        Thread.Sleep(5);
+    }
+}
---
+++ BenchmarkDotNetBugReproduction/Program.cs
@@ -0,0 +1,4 @@
+﻿using BenchmarkDotNet.Running;
+using BenchmarkDotNetBugReproduction;
+
+BenchmarkRunner.Run<Benchmarks>();
